bea061c1d368393e430248f44386ea01bbcbc70f
gagandeep kaur <gagandeepkaur15@academic.rrc.ca>
2025-09-24 15:46:36 -0500

    Added the controller for employees.

---
+++ src/api/v1/controllers/employeeController.ts
@@ -0,0 +1,30 @@
+// src/api/v1/controllers/employeeController.ts
+import { Request, Response } from "express";
+import * as employeeService from "../services/employeeService";
+
+export const getEmployees = (req: Request, res: Response) => {
+  res.json(employeeService.getAllEmployees());
+};
+
+export const getEmployee = (req: Request, res: Response) => {
+  const employee = employeeService.getEmployeeById(Number(req.params.id));
+  if (!employee) return res.status(404).send("Employee not found");
+  res.json(employee);
+};
+
+export const createEmployee = (req: Request, res: Response) => {
+  const newEmployee = employeeService.addEmployee(req.body);
+  res.status(201).json(newEmployee);
+};
+
+export const updateEmployee = (req: Request, res: Response) => {
+  const updated = employeeService.updateEmployee(Number(req.params.id), req.body);
+  if (!updated) return res.status(404).send("Employee not found");
+  res.json(updated);
+};
+
+export const deleteEmployee = (req: Request, res: Response) => {
+  const deleted = employeeService.deleteEmployee(Number(req.params.id));
+  if (!deleted) return res.status(404).send("Employee not found");
+  res.status(204).send();
+};
