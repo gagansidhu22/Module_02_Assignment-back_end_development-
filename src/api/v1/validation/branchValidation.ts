853232cc1d4099290498844402b6e11ffe4a1375
gagandeep kaur <gagandeepkaur15@academic.rrc.ca>
2025-10-07 14:45:08 -0500

    created the validation schema for branch.

---
+++ src/api/v1/validation/branchValidation.ts
@@ -0,0 +1,8 @@
+// import
+import Joi from "joi";
+
+export const branchSchema = Joi.object({
+  name: Joi.string().min(3).max(100).required(),
+  address: Joi.string().min(5).max(200).required(),
+  phone: Joi.string().pattern(/^[0-9]{10}$/).required(),
+});
