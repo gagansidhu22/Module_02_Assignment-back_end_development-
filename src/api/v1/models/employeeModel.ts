fe3b90933c8b3c19496c6a5129e7166b9093413d
gagandeep kaur <gagandeepkaur15@academic.rrc.ca>
2025-10-13 14:57:01 -0500

    Added employee model.

---
+++ src/api/v1/models/employeeModel.ts
@@ -0,0 +1,10 @@
+// src/api/v1/models/employeeModel.ts
+export interface Employee {
+  id: number;
+  name: string;
+  position: string;
+  department: string;
+  email: string;
+  phone: string;
+  branchId: number;
+}
